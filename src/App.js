9a4889fd0e4a088b8cfd5fd93b4e494c4aefc38d
Mauricio Romero <mauricionromero@hotmail.com>
2022-02-16 17:14:42 -0300

    centered add and edit componets

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Modal from "./components/Modal";
 import Main from "./components/Main";
 import Add from "./components/Add";
 import Edit from "./components/Edit";
@@ -8,7 +7,6 @@ import Edit from "./components/Edit";
 function App() {
   return (
     <BrowserRouter>
-      <Modal />
       <Routes>
         <Route path="/" element={<Main />}>
           <Route path="add" element={<Add />} />
--- src/components/Modal/index.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import styles from './Modal.module.scss'
-
-export default function index() {
-  return (
-    <div className={styles.overlay} ></div>
-  )
-}
